cdb39b9b3b8b1802a5f22ae279cd58ac390902d5
Gonçalo Baltazar <me@goncalomb.com>
2018-02-08 12:40:36 +0000

    Add marquee animation

--- src/dashwood.js
+++ src/dashwood.js
@@ -28,41 +28,91 @@
         this._elText = dashwood.createElement("div", this._el, "text");
         this._elLine = dashwood.createElement("div", this._el, "line");
         this._elArrow = dashwood.createElement("div", this._el, "arrow arrow-animation");
-        this._elLine.style.width = "25px";
+        this._timeout = 0;
+        this.waiting();
+    }
+
+    samaritan.marquee.prototype._setMode = function(mode) {
+        clearTimeout(this._timeout);
+        this._elText.style.opacity = "";
+        if (mode == "banner") {
+            this._el.classList.add("mode-banner");
+        } else {
+            this._el.classList.remove("mode-banner");
+        }
+        if (mode == "text") {
+            this._el.classList.add("mode-text");
+        } else {
+            this._el.classList.remove("mode-text");
+            this._elLine.style.width = "";
+        }
+    }
+
+    samaritan.marquee.prototype._resizeUnderline = function(off) {
+        var w = parseFloat(window.getComputedStyle(this._elText).width);
+        this._elLine.style.width = (w + off) + "px";
     }
 
     samaritan.marquee.prototype.clear = function() {
-        this._el.classList.remove("mode-banner", "mode-text");
-        this._elLine.style.width = "25px";
-        setTimeout(() => {
+        this._setMode(null);
+        if (this._elText.innerText == "-") {
             this._elText.innerText = "";
-        }, 200);
+        } else {
+            this._timeout = setTimeout(() => {
+                this._elText.innerText = "";
+            }, 200);
+        }
     }
 
     samaritan.marquee.prototype.banner = function(value) {
         if (!value) return this.clear();
-        // disable text mode
-        this._el.classList.remove("mode-text");
-        this._elLine.style.width = "25px";
-        // enable banner mode
-        this._el.classList.add("mode-banner");
+        this._setMode("banner");
         this._elText.innerText = value;
     }
 
-    samaritan.marquee.prototype.animate = function(value) {
-        if (!value) return this.clear();
+    samaritan.marquee.prototype.animate = function(text, staticEnd) {
+        if (!text) return this.clear();
+        this._setMode("text");
+        this._elArrow.classList.remove("arrow-animation");
+        var parts = text.trim().replace(/\s+/g, " ").split(" ");
+        var i = 0;
+        var loop0 = () => {
+            this._elText.style.opacity = "0";
+            this._elText.innerText = parts[i];
+            this._resizeUnderline(10);
+            this._timeout = setTimeout(loop1, 100);
+        };
+        var loop1 = () => {
+            this._elText.style.opacity = "";
+            if (++i < parts.length) {
+                this._timeout = setTimeout(loop0, 400);
+            } else if (staticEnd) {
+                this._timeout = setTimeout(() => {
+                    this._elArrow.classList.add("arrow-animation");
+                }, 100);
+            } else {
+                this._timeout = setTimeout(() => {
+                    this.waiting();
+                }, 400);
+            }
+        };
+        this._timeout = setTimeout(loop0);
+    }
+
+    samaritan.marquee.prototype.waiting = function(text) {
+        this._setMode("text");
+        this._elText.innerText = "-";
+        this._elText.style.opacity = "0";
+        this._elLine.style.width = "";
+        this._elArrow.classList.add("arrow-animation");
     }
 
     samaritan.marquee.prototype.static = function(value) {
         if (!value) return this.clear();
-        // disable banner mode
-        this._el.classList.remove("mode-banner");
-        // enable text mode
-        this._el.classList.add("mode-text");
+        this._setMode("text");
         this._elText.innerText = value;
-        // set line size
-        var w = parseFloat(window.getComputedStyle(this._elText).width);
-        this._elLine.style.width = w + 80 + "px";
+        this._resizeUnderline(80);
+        this._elArrow.classList.remove("arrow-animation");
     }
 
 })();
